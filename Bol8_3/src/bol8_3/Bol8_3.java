f52e5ae33d1124dc3506b453489c49f09ee2f54b
Leila <Leila@DESKTOP-LU1NCHG>
2019-11-19 17:29:42 +0100

    Bol8-3

---
+++ Bol8_3/src/bol8_3/Bol8_3.java
@@ -0,0 +1,25 @@
+package bol8_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Leila
+ */
+public class Bol8_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+        DiaMes obj = new DiaMes();
+        obj.DiaMes(num);
+
+        
+    }
+    
+}
---
+++ Bol8_3/src/bol8_3/DiaMes.java
@@ -0,0 +1,58 @@
+package bol8_3;
+
+/**
+ *
+ * @author Leila
+ */
+public class DiaMes {
+    
+    
+    
+  public void DiaMes(int num) {
+
+        switch (num) {
+            case 1:
+                System.out.println("Enero tiene 31");
+                break;
+            case 2:
+                System.out.println("Febrero tiene 28");
+                break;
+            case 3:
+                System.out.println("Marzo tiene 31");
+                break;
+            case 4:
+                System.out.println("Abril tiene 30");
+                break;
+            case 5:
+                System.out.println("Mayo tiene 31");
+                break;
+            case 6:
+                System.out.println("Junio tiene 30");
+                break;
+            case 7:
+                System.out.println("Julio tiene 31");
+                break;
+            case 8:
+                System.out.println("Agosto tiene 31");
+                break;
+            case 9:
+                System.out.println("Septiembre tiene 30");
+                break;
+            case 10:
+                System.out.println("Octubre tiene 31");
+                break;
+            case 11:
+                System.out.println("Noviembre tiene 30");
+                break;
+            case 12:
+                System.out.println("Diciembre tiene 31");
+                break;
+            default:
+                System.out.println("No es un mes");
+        }
+
+    }
+
+}
+
+
